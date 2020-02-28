55ec73b752b0964e1e189f5fb3a5fffc5e91848a
Mandaar Jani <mandaarj@cybage.com>
2020-02-28 13:58:54 +0530

    Sample Console App using CodeFirst Approach

---
+++ EFCoreDemoContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(75)]
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+    public class Book
+    {
+        public int BookId { get; set; }
+        [StringLength(255)]
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+    public class EFCoreDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Persist Security Info=True;Initial Catalog=EFCoreDemoConsoleApp;Data Source=MANDAARJ\SQL2017;User ID=sa;Password=cybage@123;");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EFCoreDemoContext())
+            {
+                var author = new Author
+                {
+                    FirstName = "Mandaar",
+                    LastName = "Jani",
+                    Books = new List<Book>
+                    {
+                        new Book { Title = "Hamlet" },
+                        new Book { Title = "Othello" },
+                        new Book { Title = "MacBeth" }
+
+
+                    }
+                };
+                context.Add(author);
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
